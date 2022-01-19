2ef5e99b1ce259f569141f654b05255cc166ccfe
ChickenPlays14 <chloe@chickenplays.co.uk>
2022-01-19 23:38:41 +0000

    module work

--- gadget2f.py
+++ gadget2f.py
@@ -1,4 +1,15 @@
-import random
+# import random
 
 def start():
-    print("Ayup there mate")
+    responseS1 = input("Ayup there mate! ")
+
+    if responseS1 == "hi":
+        print("It is me again!")
+        print("I've had a bit of an upgrade since the last time we talked!")
+
+    else:
+        print("Invalid response.")
+        exit("L + Ratio")
+
+
+start()
